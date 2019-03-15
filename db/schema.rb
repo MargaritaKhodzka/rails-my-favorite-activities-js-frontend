cf4fed47582c880e72ce0e57c26d2ca39cb7f24d
MargaritaKhodzka <mkhodzka@gmail.com>
2019-03-15 19:31:40 -0400

    added name column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_15_232433) do
+ActiveRecord::Schema.define(version: 2019_03_15_233109) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_03_15_232433) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
