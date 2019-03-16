2769367f6b6996aad61798c1ff3c1d69d9adf920
MargaritaKhodzka <mkhodzka@gmail.com>
2019-03-15 21:48:50 -0400

    added validations to locations and activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ApplicationRecord
   has_many :location_activities
   has_many :locations, through: :location_activities
+
+  validates :name, presence: true, uniqueness: true
+  validates :details, presence: true
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,6 @@ class Location < ApplicationRecord
   belongs_to :user
   has_many :location_activities
   has_many :activities, through: :location_activities
+
+  validates :name, presence: true
 end
