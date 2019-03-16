406d476c7735e11ad667925d41099e81e4caddc1
MargaritaKhodzka <mkhodzka@gmail.com>
2019-03-15 21:46:09 -0400

    added facebook onmiauth

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'devise'
 
 gem 'omniauth'
 gem 'omniauth-facebook'
+gem 'figaro'
+gem 'thin'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,7 +259,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, ENV['245336146276046'], ENV['16dd6311e357455a33f93d76cb16f180']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
