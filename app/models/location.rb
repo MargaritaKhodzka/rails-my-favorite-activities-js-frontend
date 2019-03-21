f93b2f4cb16ef1449fe3604214348d1f7371f9a6
MargaritaKhodzka <mkhodzka@gmail.com>
2019-03-21 14:36:41 -0400

    refactored activities_attributes method in Location model, able to edit location

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,14 +2,13 @@ class Location < ApplicationRecord
   belongs_to :user
   has_many :location_activities
   has_many :activities, through: :location_activities
-  accepts_nested_attributes_for :activities
 
   validates :name, presence: true
 
-  def activities_attributes=(activity)
-    activity.each do |k, v|
-      self.activities << Activity.find_or_create_by(name: v[:name], occurrence: v[:occurrence], details: v[:details])
-      self.activities.update(v)
+  def activities_attributes=(activities_attributes)
+    activities_attributes.values.each do |activity_attribute|
+      activity = Activity.find_or_create_by(activity_attribute)
+      self.activities << activity if activity.persisted?
     end
   end
 end
