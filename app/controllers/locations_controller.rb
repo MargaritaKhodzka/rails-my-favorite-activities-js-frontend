43b0875b87df514757a74f7a3ad69cbdc94c58e2
MargaritaKhodzka <mkhodzka@gmail.com>
2019-03-24 21:49:28 -0400

    DRY-ed up the locations controller with all_activities helper method

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,5 @@
 class LocationsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :all_activities
 
   def index
     @locations = Location.all
@@ -8,12 +8,10 @@ class LocationsController < ApplicationController
   def new
     @location = Location.new
     @location.activities.build
-    @activities = Activity.all
   end
 
   def create
     @location = Location.new(location_params)
-    @activities = Activity.all
     if @location.save
       redirect_to @location
     else
@@ -33,7 +31,6 @@ class LocationsController < ApplicationController
   def edit
     if @location = Location.find_by(id: params[:id])
       @location.activities.build
-      @activities = Activity.all
       render :edit
     else
       redirect_to locations_path
@@ -42,7 +39,6 @@ class LocationsController < ApplicationController
 
   def update
     @location = Location.find_by(id: params[:id])
-    @activities = Activity.all
     if @location.update(location_params)
       redirect_to @location
     else
@@ -54,5 +50,9 @@ class LocationsController < ApplicationController
 
   def location_params
     params.require(:location).permit(:name, :city, :state, :zip_code, :user_id, activity_ids:[], activities_attributes: %i[name occurrence details])
+  end
+
+  def all_activities
+    @activities = current_user.activities
   end
 end
