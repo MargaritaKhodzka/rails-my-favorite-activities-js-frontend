e68990c669bc1154a585f88fd0c4d65552857280
MargaritaKhodzka <mkhodzka@gmail.com>
2019-04-09 14:52:50 -0400

    added conditional statement to facebook auth method

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,11 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+
+    if @user.persisted?
+      sign_in_and_redirect @user
+    else
+      redirect_to new_user_registration_url
+    end
   end
 end
