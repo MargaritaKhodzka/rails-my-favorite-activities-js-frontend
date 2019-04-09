fad53c348b00d40292d484cccf167ef897c185ad
MargaritaKhodzka <mkhodzka@gmail.com>
2019-04-09 14:48:41 -0400

    updated Activity validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,5 +3,5 @@ class Activity < ApplicationRecord
   has_many :locations, through: :location_activities
 
   validates :name, presence: true, uniqueness: true
-  validates :details, presence: true
+  validates :details, presence: true, length: { maximum: 200, too_long: "#{count} characters is the maximum allowed" }
 end
