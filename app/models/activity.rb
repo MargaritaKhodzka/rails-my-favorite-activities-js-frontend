3fb2be9e0bd23be39f77befe27f566802f2bd19b
MargaritaKhodzka <mkhodzka@gmail.com>
2019-04-13 12:05:59 -0400

    added activity rating field to new location form

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,5 +4,5 @@ class Activity < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :details, presence: true, length: { maximum: 200, too_long: "#{count} characters is the maximum allowed" }
-  validated :rating, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 5 }
+  validates :rating, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 5 }
 end
