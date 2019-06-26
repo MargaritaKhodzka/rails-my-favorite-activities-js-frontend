0d438f815073ffe897b97a18ec52821bc158353c
MargaritaKhodzka <mkhodzka@gmail.com>
2019-06-26 14:42:13 -0400

    updated spec.md file

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@ ActiveRecord::Schema.define(version: 2019_05_20_013024) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
+    t.string "occurrence"
+    t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
