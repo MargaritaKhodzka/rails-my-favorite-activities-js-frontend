9be3c51b25f9408281ec3d58a84b36b8a39afdeb
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-02 13:29:31 -0400

    list on activities for a location populates on show page

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -22,12 +22,12 @@ const bindClickHandlers = () => {
 
   $(document).on('click', '.show-link', function(e) {
     e.preventDefault();
-    $('#app-container').html('')
     let id = $(this).attr('data-id');
     fetch(`locations/${id}.json`)
       .then(response => response.json())
       .then(location => {
         // console.log(location)
+        $('#app-container').html('')
         let newLocation = new Location(location);
         let locationHtml = newLocation.formatShow();
         $('#app-container').append(locationHtml);
@@ -63,13 +63,20 @@ function Location(location) {
 // methods on the prototype
 Location.prototype.formatIndex = function() {
   let locationHtml = `
-    <a href='/locations/${this.id}' data-id='${this.id}' class='show-link'>
-    <h5>${this.name} - ${this.city}</h5></a>
+    <ul>
+      <li><a href='/locations/${this.id}' data-id='${this.id}' class='show-link'>
+      <h5>${this.name} - ${this.city}</h5></a></li>
+    </ul>
   `
   return locationHtml;
 }
 
 Location.prototype.formatShow = function() {
+  let activitiesHtml = ``
+  this.activities.forEach(activity => {
+    activitiesHtml += `<li>${activity.name}</li>`
+  })
+
   let locationHtml = `
     <a href= '/locations/'>Back to locations</a>
     <h3>${this.name}</h3>
@@ -77,7 +84,7 @@ Location.prototype.formatShow = function() {
     <p><b>State:</b> ${this.state}</p>
     <p><b>Zip Code:</b> ${this.zip_code ? this.zip_code : ''}</p>
     <p><strong>Activities in this location:</strong></p>
-    <p>${this.activities}</p>
+    <p>${activitiesHtml}</p>
   `
   return locationHtml;
 }
