47cba1e31ccc3bdb871c9f567923640735e8fc81
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-15 11:50:29 -0400

    use es2015 Class syntax for contructor and prototype methods

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -48,46 +48,47 @@ const bindClickHandlers = () => {
   })
 }
 
-// constructor function
-function Location(location) {
-  this.id = location.id;
-  this.name = location.name;
-  this.city = location.city;
-  this.state = location.state;
-  this.zip_code = location.zip_code;
-  this.location_activities = location.location_activities;
-  this.activities = location.activities;
-}
 
-// methods on the prototype
-Location.prototype.formatIndex = function() {
-  let locationHtml = `
-    <ul>
-      <li><a href='/locations/${this.id}' data-id='${this.id}' class='show-link'>
-      <h5>${this.name} - ${this.city}</h5></a></li>
-    </ul>
-  `
-  return locationHtml;
-}
-
-Location.prototype.formatShow = function() {
-  let activitiesHtml = ``
-  if (this.activities == false) {
-    activitiesHtml += `No activities in this location yet.`;
-  } else {
-    this.activities.forEach(activity => {
-      activitiesHtml += `<li>${activity.name}</li>`;
-    });
+class Location {
+  constructor(location) {
+    this.id = location.id;
+    this.name = location.name;
+    this.city = location.city;
+    this.state = location.state;
+    this.zip_code = location.zip_code;
+    this.location_activities = location.location_activities;
+    this.activities = location.activities;
   }
 
-  let locationHtml = `
-    <a href= '/locations/'>Back to locations</a>
-    <h3>${this.name}</h3>
-    <p><b>City:</b> ${this.city}</p>
-    <p><b>State:</b> ${this.state}</p>
-    <p><b>Zip Code:</b> ${this.zip_code ? this.zip_code : ''}</p>
-    <p><strong>Activities in this location:</strong></p>
-    ${activitiesHtml}
-  `
-  return locationHtml;
+  formatIndex() {
+    let locationHtml = `
+      <ul>
+        <li><a href='/locations/${this.id}' data-id='${this.id}' class='show-link'>
+        <h5>${this.name} - ${this.city}</h5></a></li>
+      </ul>
+    `
+    return locationHtml;
+  }
+
+  formatShow() {
+    let activitiesHtml = ``
+    if (this.activities == false) {
+      activitiesHtml += `No activities in this location yet.`;
+    } else {
+      this.activities.forEach(activity => {
+        activitiesHtml += `<li>${activity.name}</li>`;
+      });
+    }
+
+    let locationHtml = `
+      <a href= '/locations/'>Back to locations</a>
+      <h3>${this.name}</h3>
+      <p><b>City:</b> ${this.city}</p>
+      <p><b>State:</b> ${this.state}</p>
+      <p><b>Zip Code:</b> ${this.zip_code ? this.zip_code : ''}</p>
+      <p><strong>Activities in this location:</strong></p>
+      ${activitiesHtml}
+    `
+    return locationHtml;
+  }
 }
