f949409e1c9f4d7b6af1892881974f65347ecbfc
MargaritaKhodzka <>
2021-04-12 20:49:27 -0400

    setting up for heroku deployment

--- db/migrate/20190626202136_remove_occurence_and_details_from_activities.rb
+++
@@ -1,6 +0,0 @@
-class RemoveOccurenceAndDetailsFromActivities < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :activities, :occurrence, :string
-    remove_column :activities, :details, :text
-  end
-end
--- db/migrate/20190626202405_remove_occurence_from_activities.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOccurenceFromActivities < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :activities, :occurence, :string
-  end
-end
